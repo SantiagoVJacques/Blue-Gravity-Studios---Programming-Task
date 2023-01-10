4a6dc2e96ffa47b53498688cb75947acaaa66b94
Santiago Villagran <72938892+SantiagoVJacques@users.noreply.github.com>
2023-01-10 00:35:15 -0600

    Added walking animations and currently working on the clothing system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed=5;
     Rigidbody2D rigidbody;
     Vector2 movement;
+    public Animator PlayerAnimator;
+    public bool isMoving = false;
     void Start()
     {
         rigidbody = this.gameObject.GetComponent<Rigidbody2D>();    
@@ -14,8 +16,36 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isMoving) 
+        {
+            PlayerAnimator.Play("Body_Idle");
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.sqrMagnitude != 0) 
+        {
+            isMoving = true;
+        }
+        if (movement.sqrMagnitude == 0) 
+        {
+            isMoving = false;
+        }
+        if (movement.x < 0) 
+        {
+            PlayerAnimator.Play("Body_Walking_Left");
+        }
+        if (movement.x > 0)
+        {
+            PlayerAnimator.Play("Body_Walking_Right");
+        }
+        if (movement.y < 0)
+        {
+            PlayerAnimator.Play("Body_Walking_Down");
+        }
+        if (movement.y > 0)
+        {
+            PlayerAnimator.Play("Body_Walking_Up");
+        }
     }
 
     private void FixedUpdate()
---
+++ Assets/Scripts/Player_Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Inventory : MonoBehaviour
+{
+    public int player_money = 100;
+    public List<string> pants = new List<string>();
+    public List<string> hats = new List<string>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
